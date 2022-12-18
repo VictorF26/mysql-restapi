079a003b5b12eabf526cd701ed07f90cad98f072
VictorF26 <fercorvictor@gmail.com>
2022-12-18 23:13:12 +0100

    security pool changes...

--- src/db.js
+++ src/db.js
@@ -5,5 +5,6 @@ export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    database: DB_DATABASE
-})
+    database: DB_DATABASE,
+    ssl: {rejectUnauthorized:true}
+});
