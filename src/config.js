b7cb624554f290a419384cf044d33af48110d6d4
VictorF26 <fercorvictor@gmail.com>
2022-12-18 23:07:06 +0100

    changes...

--- src/config.js
+++ src/config.js
@@ -8,4 +8,3 @@ export const DB_USER = process.env.DB_USER || 'p09f49vp8ui2nz4x81h7'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'pscale_pw_Hy8IExJbRqJnrscyngvjkyuzur1kTTrZIW0SktYKDAg'
 export const DB_HOST = process.env.DB_HOST || 'eu-central.connect.psdb.cloud'
 export const DB_DATABASE = process.env.DB_DATABASE || 'rest-api'
-export const DB_PORT = process.env.DB_PORT || 3306
